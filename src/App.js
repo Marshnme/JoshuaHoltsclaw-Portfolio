1d3f1c01efedd655418de4a4cf0d15b9c33e0919
Marshnm <jildsmiles@yahoo.com>
2022-02-03 22:58:59 -0500

    basic nav bar and added react-router module

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import './App.scss';
 import NavBar from './Nav/NavBar';
+import {Routes,Route} from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Routes>
+        <Route path="/" element={<App/>}/>
+        <Route path="/skills" element={<Skills/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
     </div>
   );
 }
--- src/Nav/NavBar.js
+++ src/Nav/NavBar.js
@@ -1,10 +1,13 @@
-import react from "react";
-
-
+import './NavBar.scss'
+import Link from "react-router-dom"
 const NavBar = () =>{
     return(
         <nav>
-            <h1>NavBar</h1>
+            <ul>
+                <li><Link to="/skills">Skills</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
         </nav>
     )
 }
