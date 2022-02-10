ca49c81980445bb8a845612e653cb82940e67f92
Marshnm <jildsmiles@yahoo.com>
2022-02-10 18:14:02 -0500

    Found goods slideshow tutorial starting the setup

--- src/MainContent/Projects.js
+++ src/MainContent/Projects.js
@@ -1,27 +1,61 @@
-
+import { useState } from "react"
 import "./SCSS/Projects.scss"
 import shrek from "../Imgs/shrek.png"
 import wompug from "../Imgs/wompug.jpg"
 import cow from "../Imgs/cow.jpg"
+
+
 const Projects = () => {
+    const [projects, setProjects] = useState([
+        {
+            id:0,
+            name:"RVNAV",
+            img:shrek,
+            gitHubLink:"",
+        },
+        {
+            id:1,
+            name:"Replate",
+            img:wompug,
+            gitHubLink:"",
+        },
+        {
+            id:2,
+            name:"MovieTracker",
+            img:cow,
+            gitHubLink:"",
+        },
+    ])
+
+    const [activeSlide, setActiveSlide ] = useState(projects[0])
+
+
+    let NextSlide = () =>{
+        const newIndex = activeSlide + 1;
+        setActiveSlide(newIndex);
+        
+    }
+    let PreviousSlide = () =>{
+        const newIndex = activeSlide - 1;
+        setActiveSlide(newIndex);
+        
+        
+    }
+
     return(
-        <section className="Project-container">
+        <section className="Projects-container">
             <div className="Projects-title">
                 <h2>Projects</h2>
             </div>
-            <div className="Projects-list">
-                <div>
-                    <h3>RV NAV</h3>
-                    <img className="Project-img" src={wompug} alt="wompug"/>
-                </div>
-                <div>
-                    <h3>Movie Tracker</h3>
-                    <img className="Project-img" src={shrek}/>
-                </div>
-                <div>
-                    <h3>Replate</h3>
-                    <img className="Project-img" src={cow}/>
-                </div>
+            <div className="Project-list">
+                {projects.map(project => {
+                    {console.log(project)}
+                    return(
+                    <div className="Project-card">
+                        <h3>{project.name}</h3>
+                        <img src={project.img} className="Project-img"></img>
+                    </div>)
+                })}
             </div>
         </section>
     )
